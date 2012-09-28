7d117b9501f65024915f529a8796256bc4c2c353
InternetFett <internetfett@gmail.com>
2012-09-28 00:41:19 -0500

    add story view logic and template

--- src/js/uidevtest.js
+++ src/js/uidevtest.js
@@ -12,19 +12,24 @@ $(document).ready(function() {
     
     // Import data
     $.getJSON('../js/uidevtest-data.js', function(data) {
+        var html = '';
         if(storyId) {
             // Render story view
+            storyId = parseInt(storyId.replace(/\D/g,''));
+            var story = data.objects[storyId-1];
+            story.pub_date = new Date(story.pub_date).format("h:MM t dddd, mmm. d, yyyy");
+            story.updated = new Date(story.updated).format("h:MM t dddd, mmm. d, yyyy");
+            var template = $('#storyItem').html();
+            html = Mustache.to_html(template, story);
         } else {
             // Render story list view
-            var html = '';
             $.each(data.objects, function(key, item) {
-                console.log(item);
                 item.pub_date = new Date(item.pub_date).format("h:MM t dddd, mmm. d, yyyy");
                 item.updated = new Date(item.updated).format("h:MM t dddd, mmm. d, yyyy");
                 var template = $('#storyListItem').html();
                 html += Mustache.to_html(template, item);
             });
-            $('#content').html(html);
         }
+        $('#content').html(html);
     });
 });
