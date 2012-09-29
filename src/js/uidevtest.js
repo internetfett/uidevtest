053efe8a554e8ecb913ab521f75a8524ca3cfb12
InternetFett <internetfett@gmail.com>
2012-09-29 16:12:45 -0500

    clean up comments

--- src/js/uidevtest.js
+++ src/js/uidevtest.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
         if(pair[0]=='story' && pair[1]) storyId = pair[1];
     });
     
-    // Import data
+    /*
+    * Import data and render
+    */
     $.getJSON('../js/uidevtest-data.js', function(data) {
         var html = '';
         if(storyId) {
-            // Render story view
+            // Build story view template
             storyId = parseInt(storyId.replace(/\D/g,''));
             var story = data.objects[storyId-1];
             story.pub_date = new Date(story.pub_date).format("h:MM t dddd, mmm. d, yyyy");
@@ -22,7 +24,7 @@ $(document).ready(function() {
             var template = $('#storyItem').html();
             html = Mustache.to_html(template, story);
         } else {
-            // Render story list view
+            // Build story list view template
             $.each(data.objects, function(key, item) {
                 item.pub_date = new Date(item.pub_date).format("h:MM t dddd, mmm. d, yyyy");
                 item.updated = new Date(item.updated).format("h:MM t dddd, mmm. d, yyyy");
@@ -30,6 +32,7 @@ $(document).ready(function() {
                 html += Mustache.to_html(template, item);
             });
         }
+        // Replace content wth rendered html
         $('#content').html(html);
     });
 });
